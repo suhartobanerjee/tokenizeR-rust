da6bb6f3fdbc4745ed2d480644a67eb4c4991def
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-12-13 17:59:18 +0100

    Decoder + batch decoder working as expected.

---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+use std::collections::HashMap;
+use polars::prelude::*;
+use rayon::prelude::*;
+
+
+#[cfg(test)]
+mod tests;
+
+
+fn read_vocab_dt() -> DataFrame {
+    // reading in the vocab dt
+    let result = CsvReader::from_path("./proc/bpe_vocab.tsv")
+        .expect("File not found!")
+        .with_separator("\t".as_bytes()[0])
+        .has_header(true)
+        .finish()
+        .expect("PolarsError");
+
+    return result;
+}
+
+
+// extract columns and returns as vectors.
+fn extract_columns(vocab_dt: DataFrame) -> (Vec<i64>, Vec<String>) {
+
+    let token: Vec<i64> = vocab_dt
+        .column("token")
+        .unwrap()
+        .i64()
+        .unwrap()
+        .into_iter()
+        .flatten()
+        .collect();
+
+    let sequence: Vec<String> = vocab_dt
+        .column("sequence")
+        .expect("Problems in getting the column")
+        .utf8()
+        .unwrap()
+        .into_iter()
+        .flatten()
+        .map(|seq| seq.to_owned())
+        .collect();
+
+
+    return (token, sequence);
+}
+
+
+fn build_vocab_hashmap(token: Vec<i64>, sequence: Vec<String>) -> HashMap<i64, String> {
+
+    let vocab_hashmap: HashMap<i64, String> = token.iter()
+        .zip(sequence.iter())
+        .into_iter()
+        .map(|x| (x.0.clone(), x.1.clone()))
+        .collect();
+    
+
+    return vocab_hashmap;
+}
+
+
+
+fn get_sequence_from_token(vocab_hashmap: HashMap<i64, String>,
+                           token: i64) -> String {
+
+    match vocab_hashmap.get(&token) {
+       Some(seq) => return seq.to_owned(),
+       None => return String::from("")
+    }
+}
+
+
+fn decode(tensor: Vec<i64>,
+          vocab_hashmap: HashMap<i64, String>) -> String {
+
+    let decoded_seq: String = tensor
+        .par_iter()
+        .map(|curr_token| {
+            match vocab_hashmap.get(&curr_token) {
+                Some(seq) => return seq.to_owned(),
+                None => return String::from("")
+            }
+        })
+        .collect::<Vec<String>>()
+        .join("");
+
+    return decoded_seq;
+}
+
+
+fn decode_batch(batch: Vec<Vec<i64>>,
+                vocab_hashmap: HashMap<i64, String>) -> Vec<String>{
+   
+    let batch_seq: Vec<String> = batch
+        .par_iter()
+        .map(|curr_tensor| decode(curr_tensor.to_owned(), vocab_hashmap.to_owned()))
+        .collect();
+
+    return batch_seq;
+}
---
+++ src/tests.rs
@@ -0,0 +1,75 @@
+use super::*;
+
+#[test]
+fn dt_reading() {
+    let vocab_dt = read_vocab_dt();
+    println!("{:?}", vocab_dt)
+}
+
+
+#[test]
+fn get_columns() {
+    let (token, sequence) = extract_columns(
+            read_vocab_dt()
+        );
+
+    let mut zipped = token.iter()
+        .zip(sequence.iter());
+
+    println!("{:?}", zipped.next().unwrap().1)
+}
+
+
+#[test]
+fn check_hashmap() {
+    let (token, sequence) = extract_columns(
+            read_vocab_dt()
+        );
+    let vocab_hashmap: HashMap<i64, String> = build_vocab_hashmap(token, sequence);
+
+    println!("{:?}", vocab_hashmap.get(&798).unwrap())
+}
+
+
+#[test]
+fn get_seq() {
+    let (token, sequence) = extract_columns(
+            read_vocab_dt()
+        );
+
+    let vocab_hashmap: HashMap<i64, String> = build_vocab_hashmap(token, sequence);
+
+    let seq: String = get_sequence_from_token(vocab_hashmap, 2);
+
+    println!("{}", seq);
+}
+
+
+#[test]
+fn decode_seq() {
+    let (token, sequence) = extract_columns(
+            read_vocab_dt()
+        );
+
+    let vocab_hashmap: HashMap<i64, String> = build_vocab_hashmap(token, sequence);
+
+    let decoded_seq: String = decode(vec![1, 10, 78, 95, 31_999, 2], vocab_hashmap);
+
+    println!("{}", decoded_seq);
+}
+
+
+#[test]
+fn decode_batch_seq() {
+    let (token, sequence) = extract_columns(
+            read_vocab_dt()
+        );
+
+    let vocab_hashmap: HashMap<i64, String> = build_vocab_hashmap(token, sequence);
+
+    let decoded_seq: Vec<String> = decode_batch(vec![vec![1, 8, 9, 17, 2], vec![2, 17, 9, 8, 1]], vocab_hashmap);
+
+    decoded_seq
+        .iter()
+        .for_each(|seq| println!("{}", seq));
+}
